be6127806032002105c5d6c712a3f95cdb608426
Bibek KC <bibekk49@gmail.com>
2025-04-16 13:57:52 +0545

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+
+
+
 const queryClient = new QueryClient();
 
 const App = () => (
